eab6be16e843462ae5ea751259dd69c0dd6df802
John Clarke <jp_clarke@outlook.com>
2023-08-20 09:17:31 +0000

    Using Class Components Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar.js'
+import NavBarSimple from './components/NavBarSimple.js'
 
 function App() {
   return (
     <div className={css.App}>
+      <NavBarSimple />
       <Sidebar />
     </div>
   );
