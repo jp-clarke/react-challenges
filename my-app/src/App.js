1a93c8dbafda0abebbe92c46ef1e92cda1e0f2e4
John Clarke <jp_clarke@outlook.com>
2023-09-02 11:48:56 +0000

    Hooks Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar.js'
-// import NavBarSimple from './components/NavBarSimple.js'
 import NavBarForm from './components/NavBarForm.js'
-import Content from './components/Content.js'
+import ContentHooks from './components/ContentHooks.js'
 
 function App() {
   return (
     <div className={css.App}>
-      {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHooks />
     </div>
   );
 }
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -39,7 +39,7 @@ export class Content extends Component {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
-          <h1>My Posts</h1>
+          <h1>My Photos</h1>
           <form>
             <label htmlFor="searchInput">Search:</label>
             <input
